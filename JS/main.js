1f68fbf611c9e603597638abba1a23dd304fc55d
chamachamath <mrpanda4923@gmail.com>
2024-05-21 14:31:25 +0530

    all files

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
